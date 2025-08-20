b4f9d42616a5f05a137b0ff8da7f287c298be3fd
YisusssMore <yisusssdimensional@gmail.com>
2025-08-20 00:56:42 -0500

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,12 +1,18 @@
 import discord
 import random
-import time
+import os
+from dotenv import load_dotenv
+from bot_logic import gen_pass
+
 # La variable intents almacena los privilegios del bot
 intents = discord.Intents.default()
 # Activar el privilegio de lectura de mensajes
 intents.message_content = True
 # Crear un bot en la variable cliente y transferirle los privilegios
 client = discord.Client(intents=intents)
+
+load_dotenv("C:/Users/USER/OneDrive/Desktop/Proyectos Python/Bot_discord/DiscordToken.env")
+TOKEN = os.getenv("DISCORD_TOKEN")
 
 @client.event
 async def on_ready():
@@ -23,7 +29,13 @@ async def on_message(message):
     elif message.content.startswith('$lanza_una_moneda'):
         lanzar = random.choice(["Cara","Cruz"])
         await message.channel.send(f"La moneda a caido en: {lanzar}")
+    elif message.content.startswith('$crear_contraseña'):
+        gen_pass(12)
+        password = gen_pass(12)
+        await message.channel.send(f"La contraseña es la siguiente: {password}")
     else:
         await message.channel.send(message.content)
 
-client.run("Aca el token")
+client.run(TOKEN)
+
+
